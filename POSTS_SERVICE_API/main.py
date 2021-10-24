77b6a3b988ba234e3fd1bee387e23defa87b967f
Janvi Tanniru <janvitanniru@1x-nat-vl930-172-30-121-204.wireless.umass.edu>
2021-10-24 17:42:11 -0400

    code cleanup

--- POSTS_SERVICE_API/main.py
+++ POSTS_SERVICE_API/main.py
@@ -4,7 +4,7 @@ import hashlib
 import base64
 from pydantic import HttpUrl, BaseModel
 from fastapi import FastAPI, Depends, Body, HTTPException
-from fastapi.responses import RedirectResponse, HTMLResponse
+from fastapi.responses import RedirectResponse, HTMLResponse, JSONResponse
 import random
 from fastapi.encoders import jsonable_encoder
 
@@ -28,8 +28,7 @@ def create_postid():
 async def post_info(post: Post):
     id = create_postid()
     posts.append({"pid":id, "posting":post})
-    print(posts)
-    return posts
+    return JSONResponse(status_code = 201, message="Post created successfully")
 
 @app.get('/viewpost/{postid}')
 async def get_post(postid : str):
@@ -47,34 +46,3 @@ async def get_post(postid : str):
             detail='post does not exist'
         )
     return post
-
-    
-    
-# def create_short_link(original_url: str):
-#     to_encode = f'{original_url}'
-
-#     b64_encoded_str = base64.urlsafe_b64encode(
-#         hashlib.sha256(to_encode.encode()).digest()).decode()
-#     return b64_encoded_str[:7]
-
-# @app.post('/link/shorten')
-# def get_short_link(url: HttpUrl = Body(..., embed=True)):
-#     short_link = create_short_link(url)
-#     links.append({'short_link': short_link, 'original_url': url})
-#     return links
-
-# @app.get('/{short_link}')
-# def redirect(short_link: str):
-#     # obj = db.query(ShortenedUrl).filter_by(short_link = short_link).first()
-#     short_links = [l["short_link"] for l in links]
-#     original_urls = [l["original_url"] for l in links]
-#     for i in range(len(short_links)):
-#         if short_links[i] == short_link:
-#             obj = {"short_link": short_links[i], "original_url": original_urls[i]}
-#             break
-#     if obj is None:
-#         raise HTTPException(
-#             status_code=404,
-#             detail='The link does not exist, could not redirect.'
-#         )
-#     return RedirectResponse(url=obj["original_url"])
