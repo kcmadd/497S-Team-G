b798cdb5ba70c2de57565bf6d43e6facba240363
Janvi Tanniru <janvitanniru@1x-nat-vl930-172-30-121-204.wireless.umass.edu>
2021-10-25 00:14:23 -0400

    updated post service readme.

--- POSTS_SERVICE_API/main.py
+++ POSTS_SERVICE_API/main.py
@@ -1,43 +1,46 @@
 from typing import Optional
 from fastapi import FastAPI
-import hashlib
-import base64
-from pydantic import HttpUrl, BaseModel
+from pydantic import BaseModel
 from fastapi import FastAPI, Depends, Body, HTTPException
 from fastapi.responses import RedirectResponse, HTMLResponse, JSONResponse
 import random
 from fastapi.encoders import jsonable_encoder
 
-
+# posting data content schema
 class Post(BaseModel):
     description: str
     num_rooms_available: int
     price: float
     location: str
+    duration: str
     
 app = FastAPI()
-# [{pid: str, description: str, num_rooms: int, price: double, location: str},]
-posts = []
+
+posts = [] # [{"pid": str, "posting":{ "description": str, "num_rooms_available": int, "price": float, "location": str, "duration": str}},]
+
 def create_postid():
     random_number = str(hex(random.randint(1000,9999)))
     print(random_number)
     return random_number
  
+# This endpoint for a sublessor to create a posting for a lease 
 @app.post('/createpost', status_code = 201)
 async def post_info(post: Post):
     id = create_postid()
     posts.append({"pid":id, "posting":post})
-    return JSONResponse(status_code = 201, message="Post created successfully")
+    return posts
+#JSONResponse(status_code = 201, content="Post created successfully")
 
-@app.get('/viewpost/{postid}')
+# This endpoint to view a particular post based on postid 
+@app.get('/viewpost/{postid}', status_code= 200)
 async def get_post(postid : str):
     for i in range(len(posts)):
         post_json = jsonable_encoder(posts[i])
         if post_json['pid'] == postid:
             post = post_json
         else:
-            post=None
-            
+            post = None
+    
     if post is None:
         raise HTTPException(
             status_code = 404,
