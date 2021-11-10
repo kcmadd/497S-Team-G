8847886dc8ab47ed1c01692aaa95cd594684e8df
Janvi Tanniru <janvitanniru@1x-nat-vl930-172-30-67-118.wireless.umass.edu>
2021-11-10 16:51:19 -0500

    port update

--- Event_Bus/bin/run_dev.sh
+++ Event_Bus/bin/run_dev.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker run --rm --name my_view_posts_microservice -v "$(pwd):/usr/src/app" -p 5000:5000 view_posts_microservice_dev
+docker run --rm --name my_event_bus_microservice -v "$(pwd):/usr/src/app" -p 5005:5005 event_bus_microservice_dev
