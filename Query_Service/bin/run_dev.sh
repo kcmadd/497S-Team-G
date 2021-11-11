426fe57d2c81e2622cf7b1399163a2c96f08a3c7
Aditya Narayanan <aditya.narayanan26@gmail.com>
2021-11-10 21:21:00 -0500

    minor

--- Query_Service/bin/run_dev.sh
+++ Query_Service/bin/run_dev.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker run --rm --name my_view_posts_microservice -v "$(pwd):/usr/src/app" -p 5003:5003 view_posts_microservice_dev
+docker run --rm --name my_query_service_dev -v "$(pwd):/usr/src/app" -p 5003:5003 query_service_dev
