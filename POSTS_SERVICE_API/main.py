ab1dd6ebaee3f60b0234cc09d8a4a633e46d81cc
hdhelia <harshaldhelia9@gmail.com>
2021-12-05 18:03:46 -0500

    Redis cache implemented in Posts Services.

--- POSTS_SERVICE_API/main.py
+++ POSTS_SERVICE_API/main.py
@@ -11,6 +11,8 @@ import aiohttp
 from httpx import AsyncClient
 from fastapi.encoders import jsonable_encoder
 import json
+import redis
+import pickle
 
 # posting data content schema
 class Post(BaseModel):
@@ -37,6 +39,7 @@ app = FastAPI()
 client = AsyncClient()
 
 posts = [] # {"uid": str “pid”: str, “posting” : {“amenities” : str, “num_rooms_availables” : int, “price” : float, “Restrictions”: str, “lease_duration”: str, “street_address” : str, “city”: str, “state”: str, “country”: str}}
+r = redis.StrictRedis(host='redis-service', port=6379, db=0)
 
 def create_postid():
     random_number = str(hex(random.randint(1000,9999)))
@@ -46,19 +49,30 @@ def create_postid():
 # This endpoint to view a particular post based on postid 
 @app.get('/viewpost/{postid}', status_code= 200)
 async def get_post(postid : str):
-    for i in range(len(posts)):
-        post_json = jsonable_encoder(posts[i])
-        if post_json['pid'] == postid:
-            post = post_json
-        else:
-            post = None
+    # for i in range(len(posts)):
+    #     post_json = jsonable_encoder(posts[i])
+    #     if post_json['pid'] == postid:
+    #         post = post_json
+    #     else:
+    #         post = None
      
-    if post is None:
-        raise HTTPException(
-            status_code = 404,
-            detail='post does not exist'
-        )
-    return post
+    # if post is None:
+    #     raise HTTPException(
+    #         status_code = 404,
+    #         detail='post does not exist'
+    #     )
+    
+    result = r.get(postid)
+    # jsonResult = pickle.loads(result)
+    # return jsonResult
+
+    if result:
+        jsonResult = pickle.loads(result)
+        return jsonResult
+    else:
+        return "Data not in redis cache."
+
+    # return post
  
 # This endpoint for a sublessor to create a posting for a lease 
 @app.post('/createpost/{userId}', status_code = 201)
@@ -76,7 +90,8 @@ async def post_info(userId: str, post: Post):
     # async with aiohttp.ClientSession(trust_env=True) as session:
     #     await session.post("http://event_bus:5005/events", json=event, ssl=False)
     # await requests.post("http://localhost:5005/events", json=event)
-
+    strData = pickle.dumps(data)
+    r.set(id, strData)
     return event
 
 @app.post('/events', status_code = 200)
